5e93225c55f8a0499767022f31ca505b2ed7482e
Ryan Erno <blackblad117@gmail.com>
2024-07-14 17:13:46 -0400

    SingletonTester.java

---
+++ SingletonTester.java
@@ -0,0 +1,25 @@
+package com.gamingroom;
+
+/**
+ * A class to test a singleton's behavior
+ * 
+ * @author coce@snhu.edu
+ */
+public class SingletonTester {
+
+	public void testSingleton() {
+		
+		System.out.println("\nAbout to test the singleton...");
+		
+		//Will reference back to GameService.java and will only provide One Instance of GameService.
+		// will allow for a single point of access to data
+		GameService service = GameService.getInstance();
+		
+		// a simple for loop to print the games
+		for (int i = 0; i < service.getGameCount(); i++) {
+			System.out.println(service.getGame(i));
+		}
+
+	}
+	
+}
